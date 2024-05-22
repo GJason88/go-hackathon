1183f24be9a5c19eb4a3f72feb63afcbb38ead92
GJason88 <jasongin88@gmail.com>
2024-05-22 13:32:52 -0400

    clean up

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
+
+type LocationTime struct {
+	Location string
+	Time     string
+}
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
@@ -14,16 +20,38 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 	queries := r.URL.Query()
-	locations := make(map[string]string)
 
+	locations := getLocationsFromQueries(queries)
+	locationTimes := buildLocationTimes(locations)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(locationTimes); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func buildLocationTimes(locations map[string]string) []*LocationTime{
+	var locationTimes []*LocationTime
+	for key, val := range locations {
+		locationTimes = append(locationTimes, &LocationTime{
+			Location: key,
+			Time:     val,
+		})
+	}
+	return locationTimes
+}
+
+func getLocationsFromQueries(queries url.Values) map[string]string {
 	local, _ := time.LoadLocation("Local")
-	locations["Local"] = getCurTime(local)
-	var err error
 
+	locations := make(map[string]string)
+	locations["Local"] = getCurTime(local)
+
+	var err error
+	var location *time.Location
 	if queries.Has("location") {
 		reqLocations := queries["location"]
 		for _, reqLocation := range reqLocations {
-			var location *time.Location
 			if location, err = time.LoadLocation(reqLocation); err != nil {
 				locations[reqLocation] = err.Error()
 			} else {
@@ -31,15 +59,8 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
-	// b := new(bytes.Buffer)
-	// for key, val := range locations {
-	// 	fmt.Fprintf(b, "%s: %v\n", key, val)
-	// }
-	// fmt.Fprintf(w, b.String())
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(locations); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+
+	return locations
 }
 
 func getCurTime(location *time.Location) string {
