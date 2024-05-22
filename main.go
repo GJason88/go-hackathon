76a8eb9570df12325d9450c63bc51368992a3c27
GJason88 <jasongin88@gmail.com>
2024-05-22 11:00:30 -0400

    add time handler

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +13,37 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "The time is %s", time)
+	queries := r.URL.Query()
+	locations := make(map[string]string)
+
+	local, _ := time.LoadLocation("Local")
+	locations["Local"] = getCurTime(local)
+	var err error
+
+	if queries.Has("location") {
+		reqLocations := queries["location"]
+		for _, reqLocation := range reqLocations {
+			var location *time.Location
+			if location, err = time.LoadLocation(reqLocation); err != nil {
+				locations[reqLocation] = err.Error()
+			} else {
+				locations[reqLocation] = getCurTime(location)
+			}
+		}
+	}
+	// b := new(bytes.Buffer)
+	// for key, val := range locations {
+	// 	fmt.Fprintf(b, "%s: %v\n", key, val)
+	// }
+	// fmt.Fprintf(w, b.String())
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(locations); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getCurTime(location *time.Location) string {
+	return time.Now().In(location).String()
 }
 
 func main() {
